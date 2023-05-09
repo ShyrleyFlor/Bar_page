9648da78169ee056ad1dd49ad89f11fa510ad8b8
Gabriel <blasgabriel200@gmail.com>
2023-05-09 14:01:16 -0400

    material-ui

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from 'react-router-dom'
+//estilos de Material UI
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter>  
+    {/* CssBaseline sirve para que la pagina se formate de manera correcta */}
+  <CssBaseline /> 
       <App />
     </BrowserRouter>
   </React.StrictMode>
